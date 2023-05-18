d0272778348cce239dc4cbe47a4932c9b82582e9
secretariuss <secretarius@ukr.net>
2023-05-18 02:05:07 -0300

    added some function for create password

--- script.js
+++ script.js
@@ -11,39 +11,12 @@ const valuesForPasswords = {
   symbols: "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-="
 }
 
-generatePassword: () => {
-  return console.log("fdfds");
-}
-
 // Write password to the #password input
 function writePassword() {
   var passwordLength = getPassLength();
-  console.log(passwordLength);
-  // let passLength = parseInt(prompt('How many characters would you like your password to contain?'));
-  // if(passLength < 8) {
-  //   alert("Password length must be at least 8 characters")
-  // } else if (passLength >= 129) {
-  //     alert("Password length must less than 129 characters")
-  // } else {
-  //   if(passLength) {
-  //     let specialChar = confirm('Click Ok to confirm including special characters ?');
-  //     console.log(specialChar);
-  //   }
-  //   console.log(passLength);
-  //   retVal = ""
-  //   for( var i = 0, n = valuesForPasswords.uppercaseLetters.length; i < passLength; ++i) {
-  //     retVal += valuesForPasswords.uppercaseLetters.charAt(Math.floor(Math.random() * n));
-  //   }
-  //   return console.log('%cscript.js line:30 retVal', 'color: #007acc;', retVal);
-
-  // }
-  console.log("just testing!")
   var password = generatePassword(passwordLength);
-  console.log(password);
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 function generatePassword(passLength) {
@@ -57,32 +30,22 @@ function generatePassword(passLength) {
 
     if (specialChar) {
       selectedTypeOfChacters.push(...valuesForPasswords.symbols)
-
-    } if (numb) {
-      console.log(numb);
+    }
+    if (numb) {
       selectedTypeOfChacters.push(...valuesForPasswords.numbers)
     }
     if (upperCase) {
-      console.log(upperCase);
       selectedTypeOfChacters.push(...valuesForPasswords.uppercaseLetters)
     }
     if (loverCase) {
-      console.log(loverCase);
       selectedTypeOfChacters.push(...valuesForPasswords.lowercaseLetters)
     }
     stringChar += selectedTypeOfChacters.join('').toString();
     return randomPassword(passLength, stringChar);
-   
   }
   else {
-    return alert("Please add number of length password")
+    return "If you want still generate a password please press red button below!"
   }
-
-  console.log(selectedTypeOfChacters.join('').toString());
-  // for( var i = 0, n = selectedTypeOfChacters.length; i < pass; ++i) {
-  //     retVal += selectedTypeOfChacters.map((el) => el.charAt(Math.floor(Math.random() * n)));
-  //   }
-  //   console.log(retVal);
 }
 
 function getPassLength() {
@@ -98,7 +61,7 @@ function getPassLength() {
 
 function randomPassword(pass, char) {
   let generatedPass = ""
-  for( var i = 0, n = char.length; i < pass; ++i) {
+  for (var i = 0, n = char.length; i < pass; ++i) {
     generatedPass += char.charAt(Math.floor(Math.random() * n));
   }
   return generatedPass;
